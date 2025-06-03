a683f73ef011cf115cb2799f76f466b8b3c4c892
sem1312 <juanqmasante2@gmail.com>
2025-06-03 15:08:40 -0300

    big popa

---
+++ new_script.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+var speed := 200
+
+func _process(delta):
+	var direction := Vector2.ZERO
+
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+
+	position += direction.normalized() * speed * delta
