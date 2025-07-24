1dd9b46897ecdc5255b9ddf13ddaf1d8f3565335
sem1312 <juanqmasante2@gmail.com>
2025-07-24 16:38:57 -0300

    ahora si anda la gun niggas, commit by gallo

--- Assets/guns/bullet.gd
+++ Assets/guns/bullet.gd
@@ -2,18 +2,18 @@ extends Sprite2D
 
 @onready var RayCast: RayCast2D = $RayCast2D
 
-var speed: float = 10
+var speed: float = 750
+
+func _ready():
+	RayCast.enabled = true
 
 func _physics_process(delta: float) -> void:
 	global_position += Vector2(1, 0).rotated(rotation) * speed * delta
-	
+
 	if RayCast.is_colliding():
 		var collider = RayCast.get_collider()
-		if collider:
-			var is_player = collider.has("Is_player") and collider.get("Is_player")
-			if !is_player:
-				queue_free()
-
+		if collider and not collider.is_in_group("player"):
+			queue_free()
 
 func _on_distancia_timeout() -> void:
 	queue_free()
--- Scripts/gun_9_mm.gd
+++ Scripts/gun_9_mm.gd
@@ -5,31 +5,29 @@ extends Node2D
 @onready var tiempo_disparo: Timer = $tiempo_disparo
 
 const escena_bala = preload("res://Assets/guns/bullet.tscn")
-const Is_player = true
 var fire_rate: float = 0.25
 var puede_disparar: bool = true
 
 func _ready() -> void:
+	add_to_group("player") # <- agregar el arma al grupo player
 	tiempo_disparo.wait_time = fire_rate
 	tiempo_disparo.one_shot = true
 	tiempo_disparo.connect("timeout", Callable(self, "_on_tiempo_disparo_timeout"))
 
 func _physics_process(delta: float) -> void:
-	# Rotar el arma hacia el ratón suavemente
 	var objetivo_dir = (get_global_mouse_position() - global_position).angle()
 	rotacion.rotation = lerp_angle(rotacion.rotation, objetivo_dir, 6.5 * delta)
-	
+
 	if Input.is_action_just_pressed("shoot") and puede_disparar:
 		_shoot()
 		puede_disparar = false
 		tiempo_disparo.start()
 
 func _shoot():
-	print("Disparando bala")
 	var nueva_bala = escena_bala.instantiate()
 	nueva_bala.global_position = lugar_disparo.global_position
-	nueva_bala.global_rotation = lugar_disparo.global_rotation
-	get_parent().add_child(nueva_bala)
+	nueva_bala.rotation = lugar_disparo.global_rotation
+	get_tree().current_scene.add_child(nueva_bala)
 
 func _on_tiempo_disparo_timeout() -> void:
 	puede_disparar = true
